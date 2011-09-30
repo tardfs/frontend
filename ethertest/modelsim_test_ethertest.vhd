0c0f00796b5b850fefdb8708632e2ad0bb041447
tardfs <tardfs@gmail.com>
2011-09-30 16:01:48 +0400

    added modelsim test for ethertest

---
+++ ethertest/modelsim_test_ethertest.vhd
@@ -0,0 +1,67 @@
+library ieee ;
+use ieee.std_logic_1164.all ;
+use ieee.std_logic_signed.all ;
+use ieee.std_logic_arith.all ;
+
+entity test_ethertest is
+end ;
+
+architecture ar_test_ethertest of test_ethertest is
+component ethertest is
+  port (
+        
+    clock_50    : in std_logic ;
+    clock2_50   : in std_logic ;
+    clock3_50   : in std_logic ;
+		
+    key             : in std_logic_vector(3 downto 0) ;
+	
+    enet0_gtx_clk   : out std_logic ;
+    enet0_int_n     : in std_logic ;
+    enet0_link100   : in std_logic ;
+    enet0_mdc       : out std_logic ;
+    enet0_mdio      : inout std_logic ;
+    enet0_rst_n     : out std_logic ; 
+    
+    enet0_rx_clk    : in std_logic ;
+    enet0_rx_col    : in std_logic ;
+    enet0_rx_crs    : in std_logic ;
+    enet0_rx_data   : in std_logic_vector(3 downto 0) ;
+    enet0_rx_dv     : in std_logic ;
+    enet0_rx_er     : in std_logic ;
+    
+    enet0_tx_clk    : in std_logic ;
+    enet0_tx_data   : out std_logic_vector(3 downto 0) ;
+    enet0_tx_en     : out std_logic ;
+    enet0_tx_er     : out std_logic ;
+    enetclk_25      : in std_logic  
+		 ) ;
+end component ethertest ;
+signal clock_50    :std_logic ;
+signal clock2_50   :std_logic ;
+signal clock3_50   :std_logic ;
+		
+    key             : in std_logic_vector(3 downto 0) ;
+	
+    enet0_gtx_clk   : out std_logic ;
+    enet0_int_n     : in std_logic ;
+    enet0_link100   : in std_logic ;
+    enet0_mdc       : out std_logic ;
+    enet0_mdio      : inout std_logic ;
+    enet0_rst_n     : out std_logic ; 
+    
+    enet0_rx_clk    : in std_logic ;
+    enet0_rx_col    : in std_logic ;
+    enet0_rx_crs    : in std_logic ;
+    enet0_rx_data   : in std_logic_vector(3 downto 0) ;
+    enet0_rx_dv     : in std_logic ;
+    enet0_rx_er     : in std_logic ;
+    
+    enet0_tx_clk    : in std_logic ;
+    enet0_tx_data   : out std_logic_vector(3 downto 0) ;
+    enet0_tx_en     : out std_logic ;
+    enet0_tx_er     : out std_logic ;
+    enetclk_25      : in std_logic  
+begin
+end architecture ar_test_ethertest ;
+

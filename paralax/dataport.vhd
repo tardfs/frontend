08895f2dbe54c9b7b9ef93f0189c37e9d7a08ef3
tardfs <tardfs@gmail.com>
2012-04-17 20:12:17 +0400

    memory connected

--- paralax/dataport.vhd
+++ paralax/dataport.vhd
@@ -7,12 +7,23 @@ entity dataport is
   port (
         data       : in std_logic_vector(31 downto 0) ;
         clk        : in std_logic ;
-		  reset      : in std_logic
+		reset      : in std_logic ;
+        sram_addr  : out   std_logic_vector(19 downto 0) ;
+        sram_dq    : inout std_logic_vector(15 downto 0) ;
+        sram_ub_n  : out std_logic ;
+        sram_lb_n  : out std_logic ;
+        sram_ce_n  : out std_logic ;
+        sram_oe_n  : out std_logic ;
+        sram_we_n  : out std_logic ;
 		) ;
 end dataport ;
 
 architecture a_dataport of dataport is
 begin
+process(clk)
+begin
+   if rising_edge(clk) then
+      
+   end if ;
+end process ;
 end architecture a_dataport ;
-
-

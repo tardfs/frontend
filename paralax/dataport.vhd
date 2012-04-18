ae411d613d18790fdebf0fbce57d87b66d0287e3
tardfs <tardfs@gmail.com>
2012-04-18 19:54:31 +0400

    dataport updated

--- paralax/dataport.vhd
+++ paralax/dataport.vhd
@@ -5,9 +5,9 @@ use ieee.std_logic_arith.all ;
 
 entity dataport is
   port (
+		reset      : in std_logic ;
         data       : in std_logic_vector(31 downto 0) ;
         clk        : in std_logic ;
-		reset      : in std_logic ;
         sram_addr  : out   std_logic_vector(19 downto 0) ;
         sram_dq    : inout std_logic_vector(15 downto 0) ;
         sram_ub_n  : out std_logic ;
@@ -15,6 +15,7 @@ entity dataport is
         sram_ce_n  : out std_logic ;
         sram_oe_n  : out std_logic ;
         sram_we_n  : out std_logic ;
+        clkx4      : in std_logic
 		) ;
 end dataport ;
 
